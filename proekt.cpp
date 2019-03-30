8061fca57b1ba2f6e2c97a3e9029ec194a680cc2
Frankenstein <44138096+konvergentk@users.noreply.github.com>
2019-03-30 11:57:35 +0300

    Create proekt.cpp

---
+++ proekt.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+struct resistor {
+	float resistance; // значение сопротивления на одном резисторе
+};
+
+int main() {
+	int voltage;
+	cin >> voltage; // вводим напряжение на всей цепи
+
+	return 0;
+}
