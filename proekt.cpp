31d3a2f3840fc9f2c45b5e60a473a99b9b3d0648
Frankenstein <44138096+konvergentk@users.noreply.github.com>
2019-04-06 22:11:17 +0300

    Update proekt.cpp

--- proekt.cpp
+++ proekt.cpp
@@ -9,14 +9,19 @@ struct resistor {
 
 int main() {
 	cout << "enter a voltage";
+
 	float voltage; //напряжение в цепи
 	cin >> voltage;
+
 	float R; //сопротивление в цепи
 	cin >> R;
+
 	float amperage; //сила тока в цепи
 	amperage = (voltage / R);
+
 	float power; //мощность цепи
 	power = voltage * amperage;
+
 	int t; //время работы
 	float energy; //работа
 	cin >> t;
